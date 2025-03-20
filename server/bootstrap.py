e3ca4e4e6ee3206af6538343f7d54505d1ce353a
2noScript <thangmk7@gmail.com>
2025-03-20 21:48:00 +0700

    init: base ui

---
+++ server/bootstrap.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from fastapi.staticfiles import StaticFiles
+
+from routers.ping import router as ping_router
+
+
+
+
+app = FastAPI()
+
+app.include_router(ping_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.mount("/", StaticFiles(directory="ui/dist", html=True), name="static")
+
+if __name__ == "__main__":
+    uvicorn.run("bootstrap:app", host="0.0.0.0", port=3000, reload=True)
