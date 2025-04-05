9c4b4a37a238dbf2f0d40ae6426515b619a795eb
2noScript <thangmk7@gmail.com>
2025-04-05 08:10:23 +0700

    test: check info task in browser

--- server/services/browser_service.py
+++ server/services/browser_service.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from utils.helper import write_config_file
 from constant import BROWSER_PORT
 from utils.commands import CamoufoxCommand
+from playwright.async_api import async_playwright
 import json
 import random
 
@@ -64,7 +65,7 @@ class BrowserService:
         return True
 
     async def get_info(self,identify:str):
-        return True
+        return await self._check_info_browser(identify)
 
     async def get_list(self):
         return await self._load_info_browser_config_list()
@@ -90,6 +91,7 @@ class BrowserService:
             port=random.randint(BROWSER_PORT[0],BROWSER_PORT[1])
             if not await self._check_port_has_used(port):
                 return port
+
     def _get_identify(self,browser_config:BrowserConfig):
         return f"{browser_config.id}_{browser_config.port}"
 
@@ -102,6 +104,7 @@ class BrowserService:
             return False
         except Exception:
             return False
+
     def _get_browser_config_json_with_identify(self, identify: str) -> dict:
         try:
             config_path = _browser_lunchers_dir.joinpath(f"{identify}.json")
@@ -118,3 +121,44 @@ class BrowserService:
             }
         except (json.JSONDecodeError, ValueError, IndexError):
             return {}
+    
+    async def _check_info_browser(self, identify: str):
+        id, port = identify.split('_')
+        connect_url = f"ws://localhost:{port}/{id}"
+        async with async_playwright() as p:
+            browser = await p.firefox.connect(connect_url)
+            context= await browser.new_context()
+            page=await context.new_page()
+            await page.goto("https://example.com")
+
+            context_count = len(browser.contexts)
+            context_info=[]
+            for context in browser.contexts:
+                
+                page_info=[]
+                for page in context.pages:
+                    page_info.append({
+                        "url": page.url,
+                        "title": await page.title(),
+                        "client": {
+                            "ip": await page.evaluate("() => fetch('https://api.ipify.org?format=json').then(r => r.json())"),
+                            "user_agent": await page.evaluate("() => navigator.userAgent")
+                        }
+                    })
+                context_info.append({
+                    "page_count":len(context.pages),
+                    "page_info":page_info
+                })
+
+
+ 
+            await browser.close()
+            return {
+                "connected": True,
+                "url": connect_url,
+                "context_count": context_count,
+                "context_info": context_info
+            }
+
+
+
