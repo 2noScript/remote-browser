bd317e2e6146789202cb8cd2815e2c6318603275
2noScript <thangmk7@gmail.com>
2025-04-17 08:07:39 +0700

    chore: update docker file

--- server/lib/helper.py
+++
@@ -1,7 +0,0 @@
-import aiofiles
-from pathlib import Path
-
-
-async def write_config_file(path: Path, name: str, content: str):
-    async with aiofiles.open(f"{path.resolve()}/{name}.json", "w") as f:
-        await f.write(content)
